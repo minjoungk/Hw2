ac076b542e9330ef38ed0daa89530572e67d3ffc
minjoungk <minjoungk@gmail.com>
2020-10-03 12:17:35 -0700

    hw2

---
+++ hw2.py
@@ -0,0 +1,47 @@
+#import socket module
+from socket import *
+import sys # In order to terminate the program
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#Prepare a sever socket
+serverPort = 4545
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print 'Server is on:',serverPort
+
+while True:
+
+ #Establish the connection
+ print('Ready to serve...')
+ connectionSocket, addr = serverSocket.accept()
+
+ try:
+
+    message = connectionSocket.recv(1024)
+    filename = message.split()[1]
+    f = open(filename[1:])
+    outputdata = f.read()
+
+    #Send one HTTP header line into socket
+
+
+    connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
+   
+
+    #Send the content of the requested file to the client
+    for i in range(0, len(outputdata)):
+        connectionSocket.send(outputdata[i])
+
+
+    connectionSocket.close()
+ except IOError:
+    #Send response message for file not found
+
+    connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
+
+    #Close client socket
+    connectionSocket.close()
+
+serverSocket.close()
+sys.exit()#Terminate the program after sending the corresponding data
